8a80a5fce1e003ea9d2e5a8bd6e732eaf2172050
alicesan <mk.alice02@gmail.com>
2019-04-26 16:30:04 +0900

    Fix SESSION_VARIABLES override logic

--- nifcloud/__init__.py
+++ nifcloud/__init__.py
@@ -1,3 +1,3 @@
-from . import auth, loaders, parsers, serialize, session  # noqa: F401
+from . import auth, loaders, parsers, serialize, session, configprovider  # noqa: F401
 
 __version__ = '0.1.7'
