93bc2525ed234e340b5679e89ce1401337d23a10
o108minmin <o108minmin@gmail.com>
2021-09-10 21:40:25 +0900

    add parser.py unittests(ess, dns)

--- tests/unit/test_parsers.py
+++ tests/unit/test_parsers.py
@@ -11,7 +11,7 @@ def test_parse_timestamp_is_none():
     assert cqp.parse_timestamp("") is None
 
 
-@pytest.mark.parametrize('val, expected', [
+@pytest.mark.parametrize('input, expected', [
     # computing https://pfs.nifcloud.com/api/rest/DescribeInstances.htm
     ("2010-05-17T11:22:33.456+09:00", datetime(2010, 5, 17, 11, 22, 33, 456000, tz.gettz('Asia/Tokyo'))),
     # rdb https://pfs.nifcloud.com/api/rdb/DescribeDBInstances.htm
@@ -19,8 +19,12 @@ def test_parse_timestamp_is_none():
     # nas https://pfs.nifcloud.com/api/nas/DescribeNASInstances.htm
     ("2016-02-02T09:07:40.000+09:00", datetime(2016, 2, 2, 9, 7, 40, 0, tz.gettz('Asia/Tokyo'))),
     # hatoba https://pfs.nifcloud.com/api/kubernetes-service-hatoba/ListClusters.htm
-    ("2019-07-24T00:00:00Z", datetime(2019, 7, 24, 0, 0, 0, 0, tz.gettz('UTC')))
+    ("2019-07-24T00:00:00Z", datetime(2019, 7, 24, 0, 0, 0, 0, tz.gettz('UTC'))),
+    # ess https://pfs.nifcloud.com/api/ess/GetSendStatistics.htm
+    ("2014-03-10T18:30:00Z", datetime(2014, 3, 10, 18, 30, 0, 0, tz.gettz('UTC'))),
+    # dns https://pfs.nifcloud.com/api/dns/GetChange.htm
+    ("2021-06-17T08:53:44.110Z", datetime(2021, 6, 17, 8, 53, 44, 110000, tz.gettz('UTC')))
 ])
-def test_parse_timestamp_service_format(val, expected):
+def test_parse_timestamp_service_format(input, expected):
     cqp = parsers.ComputingQueryParser()
-    assert cqp.parse_timestamp(val) == expected
+    assert cqp.parse_timestamp(input) == expected
