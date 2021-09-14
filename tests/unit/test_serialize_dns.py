c05b38c0b12c2d57854dedcf3c4348e64a08b34b
o108minmin <o108minmin@gmail.com>
2021-09-14 13:46:29 +0900

    add serialize.py unittests(dns)

---
+++ tests/unit/test_serialize_dns.py
@@ -0,0 +1,82 @@
+from botocore.model import ServiceModel
+
+from nifcloud import serialize
+
+
+class TestDnsSerializer(object):
+    dns_model_metadata = {
+        "apiVersion": "2012-12-12N2013-12-16",
+        "endpointPrefix": "dns",
+        "protocol": "rest-xml",
+        "serviceAbbreviation": "dns",
+        "serviceFullName": "NIFCLOUD DNS",
+        "serviceId": "dns",
+        "signatureVersion": "v3",
+        "uid": "dns-2012-12-12N2013-12-16"
+    }
+
+    def test_DnsSerializer(self):
+        dns_model = {
+            "metadata": self.dns_model_metadata,
+            "operations": {
+                "DnsOperation": {
+                    "http": {
+                        "method": "POST",
+                        "requestUri": "/2012-12-12N2013-12-16/operation"
+                    },
+                    "input": {
+                        "locationName": "DnsOperationRequest",
+                        "shape": "DnsOperationRequest",
+                        "xmlNamespace": {
+                            "uri": "https://route53.amazonaws.com/doc/2012-12-12/"
+                        }
+                    },
+                    "name": "DnsOperation",
+                    "output": {
+                        "shape": "DnsOperationResult"
+                    }
+                },
+            },
+            "shapes": {
+                "DnsOperationRequest": {
+                    "members": {
+                        "Parametor": {
+                            "locationName": "Parametor",
+                            "shape": "String"
+                        },
+                    },
+                    "name": "DnsOperationRequest",
+                    "required": [
+                        "Name"
+                    ],
+                    "type": "structure"
+                },
+                "DnsOperationResult": {
+                    "members": {
+                        "HostedZone": {
+                            "locationName": "HostedZone",
+                            "shape": "String"
+                        }
+                    },
+                    "name": "DnsOperationResult",
+                    "type": "structure"
+                },
+                "String": {
+                    "name": "String",
+                    "type": "string"
+                },
+            }
+        }
+
+        dns_service_model = ServiceModel(dns_model)
+        params = {
+            "Parametor": "test"
+        }
+        dns_serializer = serialize.DnsSerializer()
+        res = dns_serializer.serialize_to_request(
+            params, dns_service_model.operation_model("DnsOperation"))
+        assert res["body"] == b'<DnsOperationRequest xmlns="https://route53.amazonaws.com/doc/2012-12-12/"><Parametor>test</Parametor></DnsOperationRequest>'  # noqa: E501
+        assert res["headers"] == {}
+        assert res["method"] == "POST"
+        assert res["query_string"] == {}
+        assert res["url_path"] == "/2012-12-12N2013-12-16/operation"
